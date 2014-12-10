4e71296bba839fef5c4c665337665a30596da62f
devikad <dd2708@columbia.edu>
2014-12-10 07:36:10 -0500

    added code for only conditional prob pediction

--- coocurrence.py
+++ coocurrence.py
@@ -2,17 +2,39 @@ import csv
 from itertools import product
 import operator
 import cPickle as pickle
+import random
+import time
 
-def make_coocurr_pairs():
-	with open(r"Data/train_nostop_20k2.csv") as r, open(r"Data/comb_titletag_20k2.csv", "w") as w:
-		rdr = csv.reader(r)
-		for row in rdr:
+def make_coocurr_pairs(data):
+	with open(r"Data/comb_titletag_20k2.csv", "w") as w:
+		for row in data:
         		a=row[1]
         		b=row[3]
 			#print a,b
 			for x, y in product(a.split(), b.split()):
                 		w.write("{},{}\n".format(x, y))
 
+
+def make_train_test(test_percent):
+	t1 = time.time()
+	data = []
+	with open(r"Data/train_nostop_20k2.csv") as r:
+		rdr = csv.reader(r)
+		for row in rdr:
+			data.append(row)
+	
+	random.shuffle(data)
+	total = len(data)
+	print total, "data points total"
+	test = float(test_percent)/100*total
+	print test, "test data points"
+	test_data = data[:int(test)]
+	train_data = data[int(test):]
+	print "length of test data", len(test_data)
+	print "length of train data", len(train_data)
+	t2 = time.time()
+	print "time taken to make train/test split randomly", t2-t1
+	return test_data, train_data
 
 freq = {}
 '''
@@ -83,7 +105,7 @@ def pickle_conf_supp():
 				conf_supp[word]=[]
 				conf_supp[word].append(wlist)			
 	pickle.dump(conf_supp, open("Data/conf_supp_20k2.p", "wb"))
-	print conf_supp	
+	#print conf_supp	
 
 def get_top_10_words(tag):
 	""" get top 10 words associated with a tag """
@@ -103,16 +125,110 @@ def get_top_10_words(tag):
 		print sorted_dict[i]
 	print "--------------------------------"
 
+def make_predictions(alpha, beta):
+	""" Assuming that the confidence and support data has been calculated from the training set, and pickled, and the testing data has been pickled, this function now makes tag predictions and writes them out into a csv file"""
+
+	conf_supp = pickle.load(open("Data/conf_supp_20k2.p", "rb"))
+	test_data = pickle.load(open("Data/test_data_20k2.p", "rb"))
+
+	results = []
+	
+	for row in test_data :
+		title = row[1]
+		corr_tags = row[3]
+		pred_tags = ''
+		for word in title.split():
+			if word in conf_supp:
+				rules = conf_supp[word] 
+				# each rule is a list of tag,supp,conf
+				for r in rules:
+					if r[1]>beta and r[2]>alpha and r[0] not in pred_tags.split():
+						pred_tags+=' '
+						pred_tags+=r[0]
+
+		thisrow = [title, corr_tags, pred_tags]
+		results.append(thisrow)
+
+	with open("Data/results_20k2.csv","wb") as w:
+                writer = csv.writer(w, delimiter=',')
+                for row in results:
+                        writer.writerow(row)
+
+def make_predictions_2():
+        """ Assuming that the confidence and support data has been calculated from the training set, and pickled, and the testing data has been pickled, this function now makes tag predictions and writes them out into a csv file"""
+	"""This method only uses confidence i.e. conditional probability, and simply outputs the tags having the highest conditional probabilities, compared for all the words in the title"""
+
+        conf_supp = pickle.load(open("Data/conf_supp_20k2.p", "rb"))
+        test_data = pickle.load(open("Data/test_data_20k2.p", "rb"))
+
+        results = []
+
+        for row in test_data :
+                title = row[1]
+                corr_tags = row[3]
+                pred_tags = ''
+		all_rules = []
+                for word in title.split():
+                        if word in conf_supp:
+                                rules = conf_supp[word]
+                                # each rule is a list of tag,supp,conf
+				for r in rules :
+                                	all_rules.append(r)
+		
+		#print all_rules
+		#print "---"
+		
+		sorted_tags = sorted(all_rules, key=operator.itemgetter(2), reverse=True)
+		#print sorted_tags
+		
+		'''
+		if len(sorted_tags)>2:
+                	pred_tags+=sorted_tags[0][0]+' '+sorted_tags[1][0]+' '+sorted_tags[2][0]
+		elif len(sorted_tags)>1:
+			pred_tags+=sorted_tags[0][0]+' '+sorted_tags[1][0]
+
+		elif len(sorted_tags)>0:
+			pred_tags+=sorted_tags[0][0]
+
+		'''
+		
+		taglist = []
+		lmax = min(len(sorted_tags),11)
+		for i in range(0,lmax-1):
+			
+			if sorted_tags[i][0] not in taglist:
+				taglist.append(sorted_tags[i][0])
+			if len(taglist)>4 :
+				break
+		
+		pred_tags = ' '.join(taglist)
+		
+                thisrow = [title, corr_tags, pred_tags]
+                results.append(thisrow)
+
+        with open("Data/results_20k2_onlycon.csv","wb") as w:
+                writer = csv.writer(w, delimiter=',')
+                for row in results:
+                        writer.writerow(row)
+
+
 def main():
-	#make_coocurr_pairs()
-	make_count_dict()
-	#get_top_10_words("php")
-	#get_top_10_words("sql")
-	#get_top_10_words("java")
-	#get_top_10_words("c++")
+	
 	#make_freq_dict()
+	'''	
+	test_data, train_data = make_train_test(25)
+	make_coocurr_pairs(train_data)
+	make_count_dict()
+	get_top_10_words("php")
+        get_top_10_words("sql")
+        get_top_10_words("java")
+        get_top_10_words("c++")
 	make_conf_supp_data()
-	pickle_conf_supp()	
+        pickle_conf_supp() 
+	pickle.dump(test_data, open("Data/test_data_20k2.p", "wb"))	
+	'''
+	#make_predictions(0.5,5)
+	make_predictions_2()
 
 if __name__=="__main__" :
         main()
--- f1_score_test.py
+++ f1_score_test.py
@@ -1,26 +1,38 @@
 # assume csv fil with tags and predicted
 # make overall f1 score the average of all f1 scores
+import csv
 
-tags = ["c", "c++", "java"]
-predicted = ["java", "python"]
+counter = 0
+total_f1 = 0.0
 
-tags = set(tags)
-predicted = set(predicted)
-tp = len(tags & predicted)
-fp = len(predicted) - tp
-fn = len(tags) - tp
+with open("Data/results_20k2_onlycon.csv", "rb") as r:
+	rdr = csv.reader(r)
+	for row in rdr:
+		tags = row[1]
+		predicted = row[2]
 
-print tags
-print predicted
-print "tp", tp
-print "fp", fp
-print "fn", fn
+		tags = set(tags)
+		predicted = set(predicted)
+		tp = len(tags & predicted)
+		fp = len(predicted) - tp
+		fn = len(tags) - tp
+		'''
+		print tags
+		print predicted
+		print "tp", tp
+		print "fp", fp
+		print "fn", fn
+		'''
+		if tp>0:
+    			precision=float(tp)/(tp+fp)
+    			recall=float(tp)/(tp+fn)
+    			#print "precision", precision
+    			#print "recall", recall
+    			f1 = 2*((precision*recall)/(precision+recall))
+		else:
+    			f1 = 0.0
+		
+		total_f1 += f1
+		counter += 1	
 
-if tp>0:
-    precision=float(tp)/(tp+fp)
-    recall=float(tp)/(tp+fn)
-    print "precision", precision
-    print "recall", recall
-    print 2*((precision*recall)/(precision+recall))
-else:
-    print 0
+print "F1-score on average is", total_f1/counter
